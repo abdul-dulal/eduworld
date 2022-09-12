12822fd0b6988f9a0cd8973ee4c5e533b8736f27
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-09-12 11:05:55 +0600

    project complete

--- src/Components/pages/myitem/Myitem.js
+++ src/Components/pages/myitem/Myitem.js
@@ -25,12 +25,7 @@ const Myitem = () => {
         });
         setmyItems(data);
         setLoading(true);
-      } catch (error) {
-        // if (error.response?.status === 401 || error.response.status === 403) {
-        //   // signOut(auth);
-        //   // navigate("/login");
-        // }
-      }
+      } catch (error) {}
     };
     item();
   }, []);
@@ -46,47 +41,58 @@ const Myitem = () => {
         <title>My Items</title>
         <meta name="description" content="Helmet application" />
       </Helmet>
-      <h1 className="text-3xl font-serif text-center my-10">
-        My Item : {myItems.length}
-      </h1>
-
-      {loading ? (
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-10 container">
-          {myItems.map((item) => (
-            <div className="hover:scale-[.9] duration-1000 mt-10 ">
-              <div class=" shadow-xl py-10">
-                <div className="flex justify-center">
-                  <img src={item.img} alt="Shoes" class="rounded w-72 h-72 " />
-                </div>
-                <div class="px-10 pt-6">
-                  <h2 className="text-2xl font-serif">{item.name}</h2>
-                  <p className="text-[18px] mt-1">{item.description}</p>
-                  <div className="flex justify-between mt-3 ">
-                    <p className="font-semibold font-serif">
-                      Price: $ {item.price}
-                    </p>
-                    <p className="font-semibold font-serif mr-6">
-                      Qunatity: {item.quantity}
-                    </p>
+      {myItems.length > 0 ? (
+        <>
+          <h1 className="text-3xl font-serif text-center my-10">
+            My Item : {myItems.length}
+          </h1>
+          {loading ? (
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-10 container">
+              {myItems.map((item) => (
+                <div className="hover:scale-[.9] duration-1000 mt-10 ">
+                  <div class=" shadow-xl py-10">
+                    <div className="flex justify-center">
+                      <img
+                        src={item.img}
+                        alt="Shoes"
+                        class="rounded w-72 h-72 "
+                      />
+                    </div>
+                    <div class="px-10 pt-6">
+                      <h2 className="text-2xl font-serif">{item.name}</h2>
+                      <p className="text-[18px] mt-1">{item.description}</p>
+                      <div className="flex justify-between mt-3 ">
+                        <p className="font-semibold font-serif">
+                          Price: $ {item.price}
+                        </p>
+                        <p className="font-semibold font-serif mr-6">
+                          Qunatity: {item.quantity}
+                        </p>
+                      </div>
+                      <div className="flex justify-between py-5">
+                        <p className="font-semibold font-serif">
+                          Supplier: {item.suppiler}
+                        </p>
+                        <button
+                          onClick={() => navigate(`/inventory/${item._id}`)}
+                          className="border-2 border-gray-600 h-10 w-32 text-gray-600 font-semibold rounded"
+                        >
+                          Update
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex justify-between py-5">
-                    <p className="font-semibold font-serif">
-                      Supplier: {item.suppiler}
-                    </p>
-                    <button
-                      onClick={() => navigate(`/inventory/${item._id}`)}
-                      className="border-2 border-gray-600 h-10 w-32 text-gray-600 font-semibold rounded"
-                    >
-                      Update
-                    </button>
-                  </div>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
+          ) : (
+            <Loading />
+          )}{" "}
+        </>
       ) : (
-        <Loading />
+        <h1 className="text-2xl my-28 font-serif text-center">
+          There Is No Items
+        </h1>
       )}
     </motion.div>
   );
